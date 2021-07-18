b23dea341240ce5fc768796e7867a5d80e846fdb
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2021-07-18 13:24:17 +0300

    This is insane...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{ title }}</h1>`,
+  template: `
+    <h1>{{ title }}</h1>
+    <app-habit-list></app-habit-list>
+  `,
   styles: ['h1 { color: purple }'],
 })
 export class AppComponent {
-  title = 'give-angular-a-try';
+  title = 'Habit Tracker';
 }
--- src/app/habit-list/habit-list.component.ts
+++ src/app/habit-list/habit-list.component.ts
@@ -3,18 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-habit-list',
   template: `
-    <p>
-      habit-list works!
-    </p>
+    <h2>Habits</h2>
+    <ul>
+      <app-habit-item
+        *ngFor="let habit of habits"
+        [habit]="habit"
+      ></app-habit-item>
+    </ul>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class HabitListComponent implements OnInit {
+  habits = [
+    {
+      id: 1,
+      title: 'FOO',
+    },
+    {
+      id: 2,
+      title: 'BAR',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
